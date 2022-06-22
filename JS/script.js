7f2247267a7c5ef80db0751edf2f8efb03b2637e
Aleksey <asdrogachev@gmail.com>
2022-06-22 10:56:54 +0300

    start of film app

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,21 @@
 
-console.log("lulka");
+let numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
 
-console.log('dont stare at me')
+let personalMovieDB = {
+    conut: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+let lastFilm1 = prompt('Один из последних просмотренных фильмов?', '');
+let score1 = +prompt('На сколько его оцените?', '0');
+let lastFilm2 = prompt('Один из последних просмотренных фильмов?', '');
+let score2 = +prompt('На сколько его оцените?', '0');
+
+personalMovieDB.movies[lastFilm1] = score1;
+personalMovieDB.movies[lastFilm2] = score2;
+
+console.log(personalMovieDB.movies[lastFilm1]);
+console.log(personalMovieDB.movies[lastFilm2]);
